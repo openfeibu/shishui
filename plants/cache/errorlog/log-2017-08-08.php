9e6d36e462c8289283de3e82702b7c6d1d289458
fbc <fbc@617f0b76-4dec-2946-aed7-4eb11ec04044>
2017-08-08 11:13:13 +0000

    git-svn-id: https://39.108.144.9:8443/svn/shishui@3 617f0b76-4dec-2946-aed7-4eb11ec04044

---
+++ plants/cache/errorlog/log-2017-08-08.php
@@ -0,0 +1,11 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2017-08-08 13:15:44 --> Severity: Parsing Error --> syntax error, unexpected 'shx' (T_STRING) D:\UPUPW_K2.1_64\htdocs\plants\finecms\dayrui\controllers\Search.php 24
+ERROR - 2017-08-08 13:15:45 --> Severity: Parsing Error --> syntax error, unexpected 'shx' (T_STRING) D:\UPUPW_K2.1_64\htdocs\plants\finecms\dayrui\controllers\Search.php 24
+ERROR - 2017-08-08 13:15:46 --> Severity: Parsing Error --> syntax error, unexpected 'shx' (T_STRING) D:\UPUPW_K2.1_64\htdocs\plants\finecms\dayrui\controllers\Search.php 24
+ERROR - 2017-08-08 13:15:46 --> Severity: Parsing Error --> syntax error, unexpected 'shx' (T_STRING) D:\UPUPW_K2.1_64\htdocs\plants\finecms\dayrui\controllers\Search.php 24
+ERROR - 2017-08-08 13:15:47 --> Severity: Parsing Error --> syntax error, unexpected 'shx' (T_STRING) D:\UPUPW_K2.1_64\htdocs\plants\finecms\dayrui\controllers\Search.php 24
+ERROR - 2017-08-08 13:15:47 --> Severity: Parsing Error --> syntax error, unexpected 'shx' (T_STRING) D:\UPUPW_K2.1_64\htdocs\plants\finecms\dayrui\controllers\Search.php 24
+ERROR - 2017-08-08 13:15:48 --> Severity: Parsing Error --> syntax error, unexpected 'shx' (T_STRING) D:\UPUPW_K2.1_64\htdocs\plants\finecms\dayrui\controllers\Search.php 24
+ERROR - 2017-08-08 13:15:48 --> Severity: Parsing Error --> syntax error, unexpected 'shx' (T_STRING) D:\UPUPW_K2.1_64\htdocs\plants\finecms\dayrui\controllers\Search.php 24
+ERROR - 2017-08-08 15:40:41 --> Severity: Parsing Error --> syntax error, unexpected 'gn' (T_STRING) D:\UPUPW_K2.1_64\htdocs\plants\finecms\dayrui\controllers\Search.php 31
